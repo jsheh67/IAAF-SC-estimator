31c32f387e913760ccf494cacb4e2a1f57358f0f
joseph Ramis Sheh <joesheh@josephs-MacBook-Pro-2.local>
2022-09-11 19:10:47 -0700

    100 point calculator is working

--- iaaf-scoring/src/components/Calculator.js
+++ iaaf-scoring/src/components/Calculator.js
@@ -8,19 +8,32 @@ function Calculator({resultList, setResultList}){
         mode: "onChange"
       });
 
-    let events=["100 m","200m","300m","400m","600m","800m","1000m", "1500m",
+    const events=["100 m","200m","300m","400m","600m","800m","1000m", "1500m",
                 "1 mile", "2000m", "3000m" ];
+    
+    const hundredStats={"min":16.79, "A":30.417, "c":1.92}
 
     const eventSelectionFactory=()=>{
         return(events.map(e=>{
             return(<option key={e+"key"} value={e}>{e}</option >)
         }))
     } 
+    const calcPoints=(min, a, c, time)=>{
+        console.log(min);
+        console.log(a);
+        console.log(c);
+        console.log(time);
+
+        return Math.trunc(a*(Math.pow((Math.abs(time-min)),c)));
+        // return (a*(Math.pow((time-min),c)));
+    }
 
     const onSubmit=(OBJ)=>{
+        OBJ.time = parseFloat(OBJ.time);
+        console.log(OBJ.points)
         switch(OBJ.event){
             case "100 m":
-                OBJ.points=50;
+                OBJ.points=calcPoints(hundredStats.min, hundredStats.A, hundredStats.c, OBJ.time);
                 break;
             case "200m":
                 OBJ.points=200;
@@ -39,7 +52,7 @@ function Calculator({resultList, setResultList}){
     return(
         <div className="container-md">
             <form onSubmit={handleSubmit(onSubmit)} id="calculate-points-form">
-                <div className="form-group py-3">
+                <div className="form-group py-1">
                     <label className="form-label" htmlFor="selectEvent">Select Event</label>
                     <select className="form-control" id="selectEvent"
                     {...register("event",{required:""})}>
@@ -51,22 +64,39 @@ function Calculator({resultList, setResultList}){
 
                 </div>
 
-                <div className="form-group py-3">
-                    <input {...register("gender", { required: true })} type="radio" value="Men's" /> 
-                    Men
-                    <input {...register("gender", { required: true })} type="radio" value="Women's" />
-                    Women
+                <div className="form-group ">
+                    
+                   
+                    <input className="m-1"id="men"{...register("gender", { required: true })} type="radio" value="Men's" /> 
+                    <label className="me-5" htmlFor="men">Men</label>
+                   
+                    <input className="m-1" id="women"{...register("gender", { required: true })} type="radio" value="Women's" />
+                    <label className=""htmlFor="women">Women</label>
+                    
                 </div>
 
-                <div className="form-group py-3">
+                <div className="row">
+                <div className="form-group py-3 col-6">
                     <label className="form-label" htmlFor="time">Enter Time</label>
                     <input type="text"  className="form-control" placeholder="mm:ss.ms"
-                    {...register("time",{required: "enter time"})}/>
+                        pattern="\\d{0,1,2}:{0,1}\\d{1,2}\.{0,1}\\d{0,1,2}"
+                        {...register("time")}/>
 
                 </div>
 
-                <button id = "create" type="submit" className="btn btn-primary">find points</button>
+                <div className="form-group py-3 col-6">
+                    <label className="form-label" htmlFor="time">Enter Points</label>
+                    <input type="text"  className="form-control" placeholder=""
+                        pattern="\\d{0,1,2}:{0,1}\\d{1,2}\.{0,1}\\d{0,1,2}"
+                        {...register("points")}/>
 
+                </div>
+                </div>
+
+                <div className="row">
+                <button id = "create" type="submit" className="btn col mx-3">Calculate Points</button>
+                <button id = "create" type="submit" className="btn col mx-3">Calculate Time</button>
+                </div>
             </form>
 
             <ResultTable 
