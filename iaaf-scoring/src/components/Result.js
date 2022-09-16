4c9cbd089477cbdd202468bb626da061aa5cf39b
joseph Ramis Sheh <joesheh@josephs-MacBook-Pro-2.local>
2022-09-15 19:41:45 -0700

    small display formatting changes

--- iaaf-scoring/src/components/Result.js
+++ iaaf-scoring/src/components/Result.js
@@ -6,12 +6,32 @@ function Result({r, resultList, setResultList}){
         setResultList(filteredResults);
     }
 
+    const timeFormater=(min, sec, mili)=>{
+        let result="";
+        if (min!=0){
+            result+=(min+":")
+        }
+        if(sec<10 && min!=0){
+            result+="0"+sec+"."
+        }else if(sec.length==0){
+            result+="00."
+        }else{
+            result+=sec+".";
+        }
+        if(mili.length==0){
+            result+="0"
+        }
+        result+=mili;
+
+        return result;
+    }
+
+
     return(
         <tr className="resultRow">        
             <td>{r.gender} {r.event}</td>
-            <td>{r.minutes==0 ? "" :(r.minutes+":")}
-                {(r.seconds<10 && r.minutes!=0 )?"0"+r.seconds:r.seconds}
-                {r.miliseconds==0?".0":("."+r.miliseconds)}
+            <td>
+                {timeFormater(r.minutes, r.seconds, r.miliseconds)}
             </td>
             <td>{r.points}</td>
             <td>
