a06e2aacd0611b4955d58ca9c1fe182b0f846e71
joseph Ramis Sheh <joesheh@josephs-MacBook-Pro-2.local>
2022-09-23 17:43:41 -0700

    working on dark mode

--- iaaf-scoring/src/App.js
+++ iaaf-scoring/src/App.js
@@ -4,25 +4,37 @@ import Footer from "./components/Footer";
 import Estimator from './components/Estimator';
 
 import './App.css';
-import { useState } from 'react';
-import { Routes, Route, Navigate} from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { useNavigate} from "react-router-dom";
 import Description from './components/Description';
 
 
 
 function App() {
-
   // const navigate = useNavigate();
 
   // const[currentEvent, setCurrentEvent]= useState({});
   // const[currentTime, setCurrentTime]= useState({});
   const[resultList, setResultList]=useState([]);
   const[estResults, setEstResults]=useState([]);
+  const[darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    const body = document.getElementsByTagName("body")[0];
+    if (darkMode) {
+      body.setAttribute("class", "dark-mode");
+    } else {
+      body.setAttribute("class", "light-mode");
+    }
+  }, [darkMode])
 
   return (
     <div className="App ">
-        <Header />
+        <Header 
+          darkMode={darkMode}
+          setDarkMode={setDarkMode}
+          />
        
           <Routes>
             <Route path="" element={
--- iaaf-scoring/src/components/Header.js
+++ iaaf-scoring/src/components/Header.js
@@ -1,19 +1,26 @@
 import {Link} from "react-router-dom";
 
-function Header(){
+function Header({darkMode, setDarkMode}){
 
     return(
 
-        <nav id="header" className="navbar navbar-expand bg-gradient p-2">
-            <Link className="navbar-brand text-light ps-3" to=""><h2>Track-Calculator</h2></Link>
+        <nav id="header" className="navbar navbar-expand bg-gradient p-2 ">
+           
+                <Link className="navbar-brand text-light ps-3" to=""><h2>Track-Calculator</h2></Link>
                 <ul className="navbar-nav">
                 <li className="nav-item ">
-                    <Link className="nav-link text-light" to="">IAAF Scoring </Link>
+                    <Link className="nav-link text-light " to="">IAAF Scoring </Link>
                 </li>
                 <li className="nav-item">
-                    <Link className="nav-link text-light" to="/estimator" >Time Estimator</Link>
+                    <Link className="nav-link text-light " to="/estimator" >Time Estimator</Link>
                 </li>
                 </ul>
+             
+
+                <div id="darkToggle" >
+                    <button id="modeToggle"className="h2" onClick={() => setDarkMode(!darkMode)}>{darkMode ? <i class="bi bi-sun"></i> : <i className="bi bi-moon-fill"></i>}</button>   
+                </div>
+            
         </nav>
        
     )   
